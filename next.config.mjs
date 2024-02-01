156da05df0c2365f9bf253a5e78bcdb3b817e82f
Gabriel Maia <gabrielsena@biof.ufrj.br>
2024-02-01 04:07:57 +0000

    [dotenv] next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -6,17 +6,7 @@ const nextConfig = {
 	},
 	env: {
 		/** GitHub username loaded in build time. */
-		GITHUB_USERNAME: await fetch('https://api.github.com/user',
-			{
-				headers: {
-					Authorization: `token ${process.env.GH_TOKEN}`,
-				},
-				next: {
-					// No revalidation needed. It is fine to get it on build time and use it forever.
-					tags: ['github', 'github-username'],
-				}
-			}
-		).then(res => res.json()).then(data => data.login),
+		GITHUB_USERNAME: 'saint-lag'
 	},
 	images: {
 		remotePatterns: [
